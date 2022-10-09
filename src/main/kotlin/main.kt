4d0d48ab9d947ea5c775936d0c01044e2ab6bae7
Лисин Виталий <lega997@gmail.com>
2022-10-10 00:14:40 +0300

    Добавлено новое условие

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 package ru.netology
 
 fun main() {
-    val likes = 1
-    val result = if (likes == 1 || likes == 101 || likes == 201) {
+    val likes = 100
+    val result = if (likes == 1 || likes % 10 == 1) {
         "понравилось $likes-му человеку"
     } else "понравилось $likes людям"
     println(result)
